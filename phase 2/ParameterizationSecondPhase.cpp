8d71cbd51041eb39c899400ed356bf0c12398004
alemtzb <alemtzb@ciencias.unam.mx>
2022-04-29 13:52:58 -0500

    commented the fil for the basic parameterization of the second phase

--- phase 2/ParameterizationSecondPhase.cpp
+++ phase 2/ParameterizationSecondPhase.cpp
@@ -1,3 +1,6 @@
+//Alejandra Martínez Blancas & Ian Xul 11/04/21 alemtzb@ciencias.unam.mx
+//// Model for second phase parameterization
+
 # include <TMB.hpp>
 # include <math.h>
 # include <iostream>
@@ -8,33 +11,32 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
     //Data
-    DATA_MATRIX(tx);
-    DATA_MATRIX(txmas);
-    DATA_VECTOR(interdepth);
-    DATA_VECTOR(t1);
-    DATA_VECTOR(t2);
-    DATA_VECTOR(Dsq);
-    DATA_VECTOR(Dsq2);
-    DATA_FACTOR(x);
-    DATA_FACTOR(y);
-    DATA_FACTOR(sq);
-    DATA_MATRIX(yearmat);
-    DATA_MATRIX(ZZ);
-    DATA_MATRIX(rainMtrxFull);
-    DATA_VECTOR(BB);
-    DATA_FACTOR(square);
+    DATA_MATRIX(tx); //species abundance matrix 
+    DATA_MATRIX(txmas); //1 + species abundance matrix
+    DATA_VECTOR(interdepth); //vector that specifies which species interactions change with soil depth
+    DATA_VECTOR(t1); //species abundance in time t
+    DATA_VECTOR(t2); //species abundance in time t +1
+    DATA_VECTOR(Dsq); //soil depth of each 10 x 10 cm square
+    DATA_VECTOR(Dsq2); //squared soil depth of each 10 x 10 cm square
+    DATA_FACTOR(x); //x coordinates of square
+    DATA_FACTOR(y); //y coordinates of square
+    DATA_FACTOR(sq); //sq holds the correspondence of each set of 20 measured subsquares to the submatrix of ZZ which needs to be used for that square
+    DATA_MATRIX(yearmat); //binary matrix for year in the study
+    DATA_MATRIX(ZZ); // ZZ is a big matrix that has the information, for each square, to do linear interpolation as a matrix multiplication
+    DATA_MATRIX(rainMtrxFull); //dispertion data obtained in phase 1
+    DATA_VECTOR(BB); //birth rate parameter estimated in phase 1 which will now use to estimate parameters b and d
+    DATA_FACTOR(square); //unique identifier of each quadrat in our data set for the calculation of the random effect
 
     //Parameters
-    PARAMETER(DD);
-    PARAMETER(theta);
-    PARAMETER(sigma); 
-    PARAMETER_VECTOR(BB2);
-    PARAMETER_VECTOR(BB3);
-    PARAMETER_VECTOR(alphas);
-    PARAMETER_VECTOR(betas);
-    PARAMETER_VECTOR(alphas2);    
-    //PARAMETER_VECTOR(betas2);
-    PARAMETER_VECTOR(ranef);
+    PARAMETER(DD); //Death rate
+    PARAMETER(theta);   //parameter for negative binomial
+    PARAMETER(sigma); //parameter for calculation of random effects
+    PARAMETER_VECTOR(BB2); //parameter b
+    PARAMETER_VECTOR(BB3); //parameter c
+    PARAMETER_VECTOR(alphas); //parameter g matrix
+    PARAMETER_VECTOR(betas); //facilitation parameter matrix
+    PARAMETER_VECTOR(alphas2); //parameter h matrix
+    PARAMETER_VECTOR(ranef); //random effect
 
     //declarations
     int numdat=t1.size();
@@ -55,7 +57,6 @@ Type objective_function<Type>::operator() ()
     vector<Type> profporbb(numdat);
 
     //bounds
-    //alphas=exp(alphas);
     betas=1/(1+exp(-betas));
     theta=exp(theta);
     DD=exp(DD)/(1+exp(DD));
@@ -63,14 +64,11 @@ Type objective_function<Type>::operator() ()
     BB3=-exp(BB3)/(1+exp(BB3))*5;
     vector<Type> ybetas(lengthpar);
     ybetas << 0, betas;
-    
-    //vector<Type> ybetas2(lengthpar);
-    //ybetas2 << 0, betas2;
+
 
     //Procedure
     
-    //Birth
-    
+    //Calculation of birthrate parameters b and c
     BBs2=yearmat*BB2;
     BBs3=yearmat*BB3;
     BBs2=BBs2*Dsq;
@@ -86,7 +84,7 @@ Type objective_function<Type>::operator() ()
     BBs1=yearmat*BB1;
     BBs=BBs1+BBs2+BBs3;
 
-    //alphas
+    //Calculation of competition parameters g and h
     vector <Type> desparams=alphas2*interdepth;
     matrix <Type> desparammat(lengthpar,2);
     desparammat.col(0) = alphas;
@@ -95,7 +93,6 @@ Type objective_function<Type>::operator() ()
     for(int i=0; i<numdat;i=i+1) {
         vector <Type> depths(2);
         depths << 1,Dsq(i);
-        //vector <Type> rowalphas = desparammat*log(depths); //feliz
         vector <Type> rowalphas = desparammat*depths; //feliz
         rowalphas = exp(rowalphas);
         vector <Type> rowtx = tx.row(i);
@@ -114,21 +111,19 @@ Type objective_function<Type>::operator() ()
     //Final calculation
     BBs= BBs + rr;
     BBs=exp(BBs);
-
+    // Individuals that survive
     surv=(1-DD)*t1;
     fac=exp(txmas*ybetas);
+    //Individuals that successfully disperse
     emigrants=BBs*t1/(1.0+finalalphas)*fac;
-    //emigrants=BBs*t1/(1.0+finalalphas);
-    
-    //vector<Type> fac_head = fac.head(6);
-    //Rcout<<fac_head<<std::endl;
 
-// This array will hold 100 sheets of 10x10 containing the proportion distributed from each square
+    
+    // This array will hold 100 sheets of 10x10 containing the proportion distributed from each square
     array<Type> sResTot(10,10,100);
     sResTot.fill(0);
 
-    // Now we fill each sheet in the sResTot by cutting out a 10x10 section of rainMtrxFull so that the source square is placed in the respective position.
 
+    // Now we fill in the sResTot array with the dispersal data obtained in phase 1
     int cont=0;
 
     matrix<Type> sbRainMtrx(10,10);
@@ -153,32 +148,30 @@ Type objective_function<Type>::operator() ()
         borde=borde + sResTot.col(kk).matrix();
     }
 
-    // Max value of borde is the reference value, it indicates what the rest should be standardized to 
+    // Max value of borde is the determining factor, it indicates what the rest should be standartized to 
     Type maxuni=borde.array().maxCoeff();
 
-    //3. Estimate dispersal for each quadrat
-    // Declare vectors which will be filled with the values for each square as necessary
-    vector<int> cx(20); // x coordinates of the 20 observed squares in the quadrat
-    vector<int> cy(20); // y coordinates of the 20 observed squares in the quadrat
-    vector<Type> zz(20); // number of emigrants from the 20 observed squares in the quadrat
-    vector<Type> zzi(100); //number of emigrants from all the 100 squares in the quadrat
-    vector<Type> immiRes(20); //number of immigrants into the 20 observed squares in the quadrat
+    // Stand in vectors which will be filled with the values for each square as necessary
+    vector<int> cx(20); 
+    vector<int> cy(20); 
+    vector<Type> zz(20); 
+    vector<Type> zzi(100); 
+    vector<Type> immiRes(20); 
 
     // sResTotRot makes it easier to acces the 10 100-long hipercolumns due to limitations in the way arrays are managed in eigen
     array<Type> sResTotRot(100,10,10);
     sResTotRot=sResTot.rotate(1);
 
-    // This for goes through all squares
+    // This for goes through all distinct squares
     for( int i=0; i<(numdat/20); i=i+1){
-        //Select the data for each square
         cx=x.segment((i*20),20);
         cy=y.segment((i*20),20);
         zz=emigrants.segment((i*20),20);
 
         //Do the interpolation calculations (refer to synthesis.R) ZZ contains several 20x100 matrices, and is subsetted so only those relevant for the quadrat are considered.
-        //These are multiplied by zz give the interpolated number of emigrants from every square in the matrix.
         zzi=ZZ.block((sq(i)-1)*100,0,100,20)*zz.matrix();
 
+        //Calculate the total dispersion within the square given the model (totalDisp) and the total production of successfully dispersed individuals within the square (totalProd)
         Type totalDisp = 0; //number of individuals that remain in the quadrat after dispersal after correcting by borde
         for(int rrow=0;rrow<10;rrow=rrow+1){
             for(int ccol=0;ccol<10;ccol=ccol+1){
@@ -188,35 +181,25 @@ Type objective_function<Type>::operator() ()
         
         Type totalProd = zzi.sum(); //total number of emigrants. totalProd-totalDisp = number of individual that leave the quadrat.
 
-        // Do the same as before (sum up the contribution to each square from all others and also correct for borders), but only for the 20 observed squares values.
+        // Once we have the 100 z values we sum up the contribution to each square from all others and also correct for borders.
         for(int kk=0; kk<20; kk=kk+1){
             immiRes(kk)=(zzi*sResTotRot.col(cx(kk)-1).col(cy(kk)-1)).sum()*(maxuni/borde(cx(kk)-1,cy(kk)-1));
         }
 
-        // Assuming that the number of seeds that immigrate to the quadrat is same number that emigrate out of it, and redistributing them equally among all the squares:
-        immiRes=immiRes+(totalProd-totalDisp)/100; //immiRes has the number of seeds that arrive to each of the 20 squares in the quadrat
+        immiRes=immiRes+(totalProd-totalDisp)/100; 
 
-        immigrants.segment((i*20),20)=immiRes; //fill up immigrants (which contains data from all quadrats)
+        immigrants.segment((i*20),20)=immiRes; 
 
     }
 
 
     // Calculate the estimate (what stays plus what comes from outside)
-    //t2est=surv+immigrants;
     t2est=surv+immigrants;
 
-    // Best I could think of but would be better just to filter out. Sums up prob. only for datum with estimates different from 0 (otherwise it doesn't make sense)
-    Type finalResult = 0; // Initialize the result var
+    Type finalResult = 0; 
     for(int i = 0; i<numdat; i=i+1){
-        //if(t2est(i)>0){
            finalResult -= dnbinom(t2(i), theta, theta/(theta+t2est(i)), 1);
-        //}
     }
-    //Rcout<<(theta/(theta+t2est(1)))<<t2est(1)<<dnbinom(t2(1), theta, theta/(theta+t2est(1)), 1)<<std::endl;
-    //Rcout<<REResult+finalResult<<std::endl;
     return REResult+finalResult;
 
-
-    //Format for reporting variables
-    //Rcout<<t2(i)<<' '<<t2est(i)<<' '<<dnbinom(t2(i), theta, theta/(theta+t2est(i)), 1)<<std::endl;
 }
